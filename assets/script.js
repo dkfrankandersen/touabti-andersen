5c06144b4e3fe2ec5b6c340b062db40d693bc4ba
Frank Andersen <mail@frankandersen.dk>
2022-03-29 23:04:27 +0200

    Updated content

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,18 @@
-$("#danish" ).click(function() {
+$("#danish").click(function() {
     $("#danish").attr('id','notactive')  
     $(".fr, :lang(fr)").fadeOut('slow');
     $(".en, :lang(en)").fadeOut('slow');
     $(".da, :lang(da)").fadeIn('slow'); 
 });
 
-$("#english" ).click(function() {
+$("#english").click(function() {
     $("#english").attr('id','notactive')  
     $(".da, :lang(da)").fadeOut('slow');
     $(".fr, :lang(fr)").fadeOut('slow');
     $(".en, :lang(en)").fadeIn('slow'); 
 });
 
-$("#french" ).click(function() {
+$("#french").click(function() {
     $("#french").attr('id','notactive')  
     $(".da, :lang(da)").fadeOut('slow');
     $(".en, :lang(en)").fadeOut('slow');
